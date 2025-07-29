0335d81e53da2e93ad73ea8d4f7cef4b59d103bc
Harsahibjit Singh <harsahibjitsingh@icloud.com>
2025-07-29 08:02:38 +0530

    idaetathon chnages

--- site/src/app/events/ideatathon/page.jsx
+++ site/src/app/events/ideatathon/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Head from 'next/head';
 import React, { useState, useEffect } from 'react'
 import { motion, useScroll, useTransform, AnimatePresence } from 'framer-motion'
 import Image from 'next/image'
